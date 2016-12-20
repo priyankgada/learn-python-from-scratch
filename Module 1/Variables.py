fa7c1a5dec19839c71aaee2af431c3e07bfef5db
Priyank Gada <kustom.hackers@gmail.com>
2016-12-20 19:07:39 +0530

    Create Variables.py

---
+++ Module 1/Variables.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# www.youtube.com/priyankgada
+# WWW.PRIYANKGADA.COM
+# Variables - Lesson 4.
+x = z = w = 5
+y = 1.2
+a = "Priyank Gada "
+b = "www.priyankgada.com"
+container = " This is a container  it contains data"
+d, e, f = 5, 1.2, " BITCH "
+g = x
+
+print x
+print y
+print a
+print b
+print z
+print w 
+print container
+print d
+print e
+print f 
+print g
+
