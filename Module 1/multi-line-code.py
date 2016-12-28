22a2952a80ff8ef0f3798ce9a6cc7b1a72d9052f
Priyank Gada <kustom.hackers@gmail.com>
2016-12-28 20:19:21 +0530

    Create multi-line-code.py

---
+++ Module 1/multi-line-code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+x = 1
+y = 2
+z = 3
+a = x +\
+ y +\
+  z 
+print a
+
+print " www.priyank\
+gada.com "
+
+#multiline code : You can break your code using a \ and start writing the code on  a new line.
+
