f2300aa7d0c81ca9769c08e22e906a0f377e09ba
Priyank Gada <kustom.hackers@gmail.com>
2016-12-28 20:40:55 +0530

    Create Challenge 1.py

---
+++ Challenges/Challenge 1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+a = int(raw_input("Enter First Number "))
+b = int(raw_input("Enter Second Number "))
+c=a+b
+print c 
+# Question : Design an application that can take input and calculate additional_tests
+
+# Shorten the code ! 
+#www.priyankgada.com
+#www.youtube.com/priyankgada
+
+
+
+
+
+
+
+
+
+
+
+
+
