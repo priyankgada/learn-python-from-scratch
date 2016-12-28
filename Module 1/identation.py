abc7d16f33b5eddc2969700ca1625a9ef57e5e97
Priyank Gada <kustom.hackers@gmail.com>
2016-12-28 20:13:33 +0530

    Create identation.py

---
+++ Module 1/identation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+if True:
+    print " We are inside True "
+    print " We are inside true 2"
+    print " Indent ";
+else:
+    print " We are inside false / else "
+print " Outside if else "
+
+#indentation : You have to write the code in proper structure . ; is not 
+# Else should be behind if . Nothing should be between them.
+
+#www.youtube.com/priyankgada
+#www.priyankgada.com
