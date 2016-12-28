e1d31c099f2d3df42d1a4caa4362ad0754dc39b3
Priyank Gada <kustom.hackers@gmail.com>
2016-12-28 20:06:28 +0530

    Create Reserved Keywords.py

---
+++ Module 1/Reserved Keywords.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+#Reserved Keywords are keywords that can not be used as variables ......
+# By default reserved for some Action
+
+#and
+#assert
+#break
+#class
+#exec
+#finally:
+ #   for
+ #   from
+ #   global
+ #   if
+ #   elif
+ #   else
+ #   import
+   # is
+  #  in
+ #   lambda
+ #   not 
+#    or
+ #   pass
+#print
+#raise
+#try
+#while
+#with
+#yield
+#continue
