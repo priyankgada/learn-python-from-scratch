56ad10ff02130c60c23020ac51af448d8d630e19
Priyank Gada <kustom.hackers@gmail.com>
2017-03-30 09:47:41 +0530

    Create raw_input.py

---
+++ Module 1/raw_input.py
@@ -0,0 +1,13 @@
+#/usr/bin/python
+
+col = raw_input(" Enter Your Fav Number :")
+d = raw_input()
+print " Fav Number is  : " + col
+print d
+
+#raw_input("") allows us to take input. raw_input() to print nothing and take 
+#input only .
+#raw_input("Something ")
+
+#www.priyankgada.com
+#www.youtube.com/priyankgada
