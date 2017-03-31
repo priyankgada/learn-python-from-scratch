d12265599fbc136207650201dfed44194fab0df9
Priyank Gada <kustom.hackers@gmail.com>
2017-03-31 07:38:38 +0530

    Create Challenge 1-addition.py

---
+++ Module 1/Challenge 1-addition.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# www.youtube.com/priyankgada
+# WWW.PRIYANKGADA.COM
+# Question : Take input from user and add two numbers .
+
+a = int(raw_input("Enter First Number "))
+b = int(raw_input("Enter Second Number "))
+c = a+b
+print c
