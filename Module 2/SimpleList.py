f225263f827c4f15cb3bcc9c25299cd64686c0d1
Priyank Gada <kustom.hackers@gmail.com>
2018-03-07 10:10:10 +0530

    Create SimpleList.py
    Here we will learn how lists work in python

---
+++ Module 2/SimpleList.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#www.youtube.com/priyankgada
+
+string = ['Priyank','1','1.2','4.22','Gada']
+string2 = ['Dont','Forget','to','Subscribe']
+print string 
+print string[0]
+print string[4] #print 4th item of the list
+print string[2:4] #slicing to print 2 to 4 items of string 4 is not included 
+print string[2:] #everything after 2 and 2
+print string[:4] #everything before 4 4 is not included
+print string * 4 #print multiple times
+print string + string2 #print 2 lists combined 
