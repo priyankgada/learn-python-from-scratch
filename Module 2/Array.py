3ed5bf09410800533f5043903969aae5745cdfb7
Priyank Gada <kustom.hackers@gmail.com>
2018-03-07 10:09:33 +0530

    Update Array.py
    Here we will learn slicing and stuff.

--- Module 2/Array.py
+++ Module 2/Array.py
@@ -1,1 +1,18 @@
 
+#!/usr/bin/python 
+#www.youtube.com/priyankgada
+
+#arrays in simple terms is counting or using some part of something
+
+string = "ABCDEFGHI"
+string2 = "More"
+
+print string 
+print string[0]
+print string[8] #print 1 letter or string
+print string[2:5] #slicing to print some part from 2 to 5
+print string[2:] #everything after 2 and 2
+print string[:4] #everything before 4
+print string * 4 #print multiple times
+print string + " Concatenation" #string and something
+print string + string2
