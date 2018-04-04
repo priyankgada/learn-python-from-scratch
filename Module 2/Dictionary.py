cf7f193ac0c629e3eba129d453017750984a4742
Priyank Gada <kustom.hackers@gmail.com>
2018-04-04 22:54:52 +0530

    Create Dictionary.py

---
+++ Module 2/Dictionary.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+dict1 = {}
+dict2 = {}
+dict3 = {}
+
+dict1['Word'] = "This is the meaning of the word"
+dict2['Priyank'] = "Subscribe To My Channel"
+dict3[3] = "This is third "
+
+dict4 = {'Key': 'Value1',
+         'key2': 'Value2',
+         'Key3': 123
+         }
+
+print dict1['Word']
+print dict2
+print dict4.keys()
+print dict4.values()
+#Subscribe - www.youtube.com/priyankgada
+#Full videos on my youtube channel.
