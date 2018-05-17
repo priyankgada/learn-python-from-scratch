edfc1f9c30a6a088840b0003747ae98b9774d088
Priyank Gada <kustom.hackers@gmail.com>
2018-05-17 17:15:22 +0530

    Create ForLoop.py

---
+++ Module 3/ForLoop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#www.youtube.com/priyankgada
+
+for l in 'helloWorld':
+    print " the letter is " , l
+    
+list = ['First','Second','third']
+
+for u in list:
+    print "This is ", u
+    
