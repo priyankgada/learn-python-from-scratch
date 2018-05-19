7d93089681ded4059325d2677e33a78f1b98819b
Priyank Gada <kustom.hackers@gmail.com>
2018-05-19 20:03:18 +0530

    Create break.py

---
+++ Module 3/break.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#www.youtube.com/priyankgada
+
+for l in 'helloWorld':
+    if l == 'W':
+        break
+    print " the letter is " , l
+    
+    
+list = ['First','Second','third']
+
+for u in list:
+    if u == 'Second':
+        break
+    print "This is ", u
+    
