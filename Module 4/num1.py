0e5025aeace42cf96431c71256623d2ade1bca73
Priyank Gada <kustom.hackers@gmail.com>
2018-06-23 17:03:46 +0530

    Create num1.py

---
+++ Module 4/num1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#www.youtube.com/priyankgada
+
+#numbers are datatypes
+
+x = int(4.00)
+y = int(3.19)
+int()
+long()
+float()
+complex()
+
+100
+100000000000000L
+
+#del x This will delete value.
+print y
